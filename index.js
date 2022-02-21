58c7ba0b6a048a53d865a7a3a96500fa264d7f33
Huy Vo <vohuy819@gmail.com>
2022-02-21 18:27:01 +1030

    add switch cases

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const Manager = require('./lib/Manager');
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer")
+const Engineer = require("./lib/Intern")
 fs = require('fs');
 inquirer = require('inquirer');
 
@@ -8,26 +9,53 @@ let team = [];
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 
-// Prompt question of related role
-function addManager() {
-  const newManager = new Manager
-  inquirer.prompt(newManager.getQuestion())
+// Prompt question of related role and push new member object to array 'team'
+function addMember(member) {
+  const newmember = new member
+  inquirer.prompt(newmember.getQuestion())
 .then (answers => {
   for (let i in answers) {
-    newManager[i]= answers[i] 
+    newmember[i]= answers[i] 
   }
-  console.log(answers);
-  console.log(newManager);
+  console.log(`A new member is assigned to your team`);
+  // console.log(answers);
+  // console.log(newManager);
+  team.push(newmember);
+  console.log(team);
+  addMemberCheck()
 })
 };
 
-function addEngineer() {
-  const newEngineer = new Engineer
-  team.push(newmember);
-  console.log(team);
+// Prompt question to check whether user want to add new member or finish building their team
+function addMemberCheck() {
+  inquirer.prompt(
+    {
+      type: 'list',
+      name: 'role',
+      message: `Do you want to add another member to your dream team?`,
+      choices: [`Add an engineer`,`Add an intern`,`Finish building my dream team`]
+  }, 
+  )
+  .then(ans => {
+    switch (ans) {
+      case `Add an engineer`:
+        addMember(Engineer);
+        break;
+      case `Add an intern`:
+        addMember(Intern);
+        break;
+      case `Finish building my dream team`:
+        createHtmlfile();
+        break;
+    }
+  }
+  )
 };
 
-addManager();
+function createHtmlfile(){
+  
+}
+
 
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
