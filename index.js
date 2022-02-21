7fa0d3d4d3718db6abe9a1f3203d038ce73a2393
Huy Vo <vohuy819@gmail.com>
2022-02-21 23:18:39 +1030

    complete program

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer")
-const Engineer = require("./lib/Intern")
+const Intern = require("./lib/Intern")
 fs = require('fs');
 inquirer = require('inquirer');
 
 let team = []; 
 
+function mainHtml(inner) {
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Team</title>
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+      integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+      <header class="jumbotron jumbotron-fluid bg-danger">
+          <div class="container">
+          <h1 class="display-3 text-center text-light">My Team</h1>
+          </div>
+      </header>
+      <div class="container">
+          <div class="row d-flex justify-content-center flex-wrap">
+          ${inner}
+          </div>
+      </div>
+  </body>
+  </html>`
+  return html
+} 
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
 // Prompt question of related role and push new member object to array 'team'
 function addMember(member) {
+  // console.log(member)
   const newmember = new member
+  console.log(`Please enter ${newmember.getRole()}'s infor`);
   inquirer.prompt(newmember.getQuestion())
 .then (answers => {
   for (let i in answers) {
@@ -21,7 +53,7 @@ function addMember(member) {
   // console.log(answers);
   // console.log(newManager);
   team.push(newmember);
-  console.log(team);
+  // console.log(team);
   addMemberCheck()
 })
 };
@@ -37,7 +69,7 @@ function addMemberCheck() {
   }, 
   )
   .then(ans => {
-    switch (ans) {
+    switch (ans.role) {
       case `Add an engineer`:
         addMember(Engineer);
         break;
@@ -52,18 +84,19 @@ function addMemberCheck() {
   )
 };
 
+// Create html file
 function createHtmlfile(){
-  
+  let innerHTMl ='';
+  // console.log(team)
+  team.forEach(member => {
+    // console.log(innerHTMl);
+    innerHTMl += member.html();
+    // console.log(innerHTMl);
+  })
+  // console.log(innerHTMl);
+  fs.writeFile(`./dist/index.html`, mainHtml(innerHTMl), (err) =>
+  err ? console.log(err) : console.log('Success!'))
 }
 
-
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-
+// Initialisation
+addMember(Manager);
