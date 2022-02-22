6e1545d9f21a4fe25530241fda0428caa0b18d17
Huy Vo <vohuy819@gmail.com>
2022-02-22 21:53:48 +1030

    add readme file

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer")
-const Intern = require("./lib/Intern")
+const {Manager} = require("./lib/Manager");
+const {Engineer} = require("./lib/Engineer")
+const {Intern} = require("./lib/Intern");
+const mainHtml = require("./lib/mainhtml");
 fs = require('fs');
 inquirer = require('inquirer');
-
 let team = []; 
-
-function mainHtml(inner) {
-  let html = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>My Team</title>
-      <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-      integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn"
-      crossorigin="anonymous"
-    />
-  </head>
-  <body>
-      <header class="jumbotron jumbotron-fluid bg-danger">
-          <div class="container">
-          <h1 class="display-3 text-center text-light">My Team</h1>
-          </div>
-      </header>
-      <div class="container">
-          <div class="row d-flex justify-content-center flex-wrap">
-          ${inner}
-          </div>
-      </div>
-  </body>
-  </html>`
-  return html
-} 
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // Prompt question of related role and push new member object to array 'team'
 function addMember(member) {
   // console.log(member)
-  const newmember = new member
+  const newmember = new member;
   console.log(`Please enter ${newmember.getRole()}'s infor`);
   inquirer.prompt(newmember.getQuestion())
 .then (answers => {
